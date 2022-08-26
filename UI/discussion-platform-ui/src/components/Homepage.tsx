258a66dc628ce180032baca9788fc16705383d4c
omerkucuker <omer.kucuker@vbt.com.tr>
2022-08-26 08:53:51 +0300

    ss added

--- UI/discussion-platform-ui/src/components/Homepage.tsx
+++ UI/discussion-platform-ui/src/components/Homepage.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { Navigate, useLocation, useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom';
 import { Comment } from '../model/Comment';
 import { CommentRequestModel } from '../model/CommentRequestModel';
 import { CommentUpdateRequestModel } from '../model/CommentUpdateRequestModel';
